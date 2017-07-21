6569a8a9752dacc3202be8d691ed5a2260486429
ambodin <am.bodin@hotmail.fr>
2017-07-21 11:01:19 +0200

    m

--- template-contact.php
+++ template-contact.php
@@ -30,9 +30,11 @@ Template Name: Contact
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
   </div>
 
-  <div>
+<div class="row">
+  <div class="medium-12 columns">
     <iframe class="carte" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d44460.16855328327!2d-0.5721335055730098!3d45.85609170135863!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4800e6c23c8df961%3A0xa19956271e5b107d!2s17770+Saint-Hilaire-de-Villefranche!5e0!3m2!1sfr!2sfr!4v1500117988534" width="800" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
   </div>
+</div>
 
   <?php the_content(); ?>
 
