21fe1bfd87152b3a05aa7ca53d363a5b2d697af0
ambodin <am.bodin@hotmail.fr>
2017-09-07 13:00:47 +0200

    modif page contact

--- template-contact.php
+++ template-contact.php
@@ -15,10 +15,10 @@ Template Name: Contact
         <h1 class="grand-titre">Suis nous partout ! </h1>
       </div>
       <div class="medium-2 columns" data-equalizer-watch>
-        <img class="logocontact facebook" src="<?php echo get_template_directory_uri(); ?>/assets/images/logofacebook.png">
+        <a href="https://www.facebook.com/Festival-Les-ondes-sen-m%C3%AAlent-1442703312695699/" class="logocontact facebook" target="_blank"><img class="logocontact facebook" src="<?php echo get_template_directory_uri(); ?>/assets/images/logofacebook.png" alt="vignette facebook"></a>
       </div>
       <div class="medium-2 columns" data-equalizer-watch>
-        <img class="logocontact" src="<?php echo get_template_directory_uri(); ?>/assets/images/logoyoutube.png">
+        <a href="https://www.youtube.com/channel/UCunbWVRBbJlhGEjjTLmaRxA" class="logocontact youtube" target="_blank"><img class="logocontact youtube" src="<?php echo get_template_directory_uri(); ?>/assets/images/logoyoutube.png" alt="vignette youtube"></a>
       </div>
 
     </div>
@@ -26,20 +26,19 @@ Template Name: Contact
 
 
   <div class="introduction">
-    <p class="intro">C'est à Saint-Hilaire-de-Villefranche que tu pourras nous retrouver. Cette année, le lieu se situe en Centre-ville sur la route de Saint-Savinien ! Tu peux aussi nous contacter avec le formulaire plus bas, si tu as des questions, ou juste pour laisser un petit message ! :)</p>
+    <p class="intro">C'est à Saint-Hilaire-de-Villefranche que tu pourras nous retrouver. Cette année, le lieu se situe en Centre-ville sur la route de Saint-Savinien ! Tu peux aussi nous contacter sur le formulaire plus bas, si tu as des questions, ou juste pour laisser un petit message ! :)</p>
   </div>
 
 <div class="row">
   <div class="medium-6 columns">
-    <iframe class="carte" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d44460.16855328327!2d-0.5721335055730098!3d45.85609170135863!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4800e6c23c8df961%3A0xa19956271e5b107d!2s17770+Saint-Hilaire-de-Villefranche!5e0!3m2!1sfr!2sfr!4v1500117988534" width="100%" height="500px" frameborder="0" style="border:0" allowfullscreen></iframe>
+    <iframe class="carte" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d44460.16855328327!2d-0.5721335055730098!3d45.85609170135863!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4800e6c23c8df961%3A0xa19956271e5b107d!2s17770+Saint-Hilaire-de-Villefranche!5e0!3m2!1sfr!2sfr!4v1500117988534" width="100%" height="500px" border="0" style="border:0" allowfullscreen></iframe>
   </div>
   <div class="medium-6 columns">
-    <!-- <?php echo do_shortcode('[contact-form-7 id="158" title="Contact"]'); ?> -->
-
-      <?php the_content(); ?>
+    <?php echo do_shortcode('[contact-form-7 id="158" title="Contact"]'); ?>
   </div>
 </div>
 
+  <?php the_content(); ?>
 
 
 
