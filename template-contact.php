5f8da53903461c761bc8f9490f1c2592787e6431
Ajjax <trilab@hotmail.fr>
2017-10-03 14:30:11 +0200

    commit template simple
    commit template simple

--- template-contact.php
+++ template-contact.php
@@ -6,8 +6,7 @@ Template Name: Contact
 
 <?php get_header(); ?>
 <div class="content contact row">
-  <?php if (have_posts()) {
-    the_post();
+  <?php if ( have_posts() ) : while ( have_posts() ) : the_post();
   ?>
   <div class="container">
     <div class="row" data-equalizer data-equalize-on="medium" id="test-eq">
@@ -20,31 +19,21 @@ Template Name: Contact
       <div class="medium-2 columns" data-equalizer-watch>
         <a href="https://www.youtube.com/channel/UCunbWVRBbJlhGEjjTLmaRxA" class="logocontact youtube" target="_blank"><img class="logocontact youtube" src="<?php echo get_template_directory_uri(); ?>/assets/images/logoyoutube.png" alt="vignette youtube"></a>
       </div>
-
     </div>
   </div>
-
-
   <div class="introduction">
     <p class="intro">C'est à Saint-Hilaire-de-Villefranche que tu pourras nous retrouver. Cette année, le lieu se situe en Centre-ville sur la route de Saint-Savinien ! Tu peux aussi nous contacter sur le formulaire plus bas, si tu as des questions, ou juste pour laisser un petit message ! :)</p>
   </div>
-
 <div class="row">
   <div class="medium-6 columns">
     <iframe class="carte" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d44460.16855328327!2d-0.5721335055730098!3d45.85609170135863!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4800e6c23c8df961%3A0xa19956271e5b107d!2s17770+Saint-Hilaire-de-Villefranche!5e0!3m2!1sfr!2sfr!4v1500117988534" width="100%" height="500px" border="0" style="border:0" allowfullscreen></iframe>
   </div>
   <div class="medium-6 columns">
-    <?php echo do_shortcode('[contact-form-7 id="158" title="Contact"]'); ?>
   </div>
 </div>
-
-  <?php the_content(); ?>
-
-
-
-
   <?php
-  } ?>
+endwhile;
+endif;
+   ?>
 </div>
-
 <?php get_footer(); ?>
